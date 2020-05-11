1d4f714aced55e1cd2b7ed7e4a48224fad81142b
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-11 12:26:30 +0200

    template

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
 
 
+$('.invio').click(function(){
+    // leggo il testo inserito dall'utente
+    var messaggio_proprietario = $('.testo_messaggio').val();
+    console.log(messaggio_proprietario);
+})
+
 });
