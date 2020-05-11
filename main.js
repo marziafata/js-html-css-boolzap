a0e85acbba64bff765abfcf0d223a099c9e96c30
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-11 13:01:26 +0200

    provo a creare una funzione

--- main.js
+++ main.js
@@ -34,4 +34,20 @@ $('.testo_messaggio').keypress(function(event){
             }
 })
 
+// creo una funzione per il testo del messaggio
+function input_messaggio(testo) {
+    // leggo il testo inserito dall'utente
+    var messaggioDaInviare = $('.testo_messaggio').val();
+
+    // copio l'elemento template
+    var nuovo_messaggio = $('.template .fumetto_verde.primo').clone();
+
+    // inserisco il testo letto dall'input_footer
+    nuovo_messaggio.text(messaggioDaInviare);
+
+    // appendo il nuovo messaggio
+    $(nuovo_messaggio).appendTo('.riga_messaggio.out');
+
+}
+
 })
