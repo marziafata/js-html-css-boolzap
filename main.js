e89a24ca279c7236392a747030472f0aa7b8af3d
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-11 17:34:10 +0200

    risolti problemi

--- main.js
+++ main.js
@@ -1,64 +1,50 @@
 $(document).ready(function() {
 
-
 $('.invio').click(function(){
+    invia_messaggio();
+})
+
+$('.testo_messaggio').function(event){
+            var keycode = (event.keyCode ? event.keyCode : event.which);
+            // if (event.keyCode) {
+            //     keycode = event.keyCode;
+            // } else {
+            //     keycode = event.witch;
+            // }
+            if(keycode == '13'){
+                invia_messaggio();
+            }
+})
+
+
+function invia_messaggio() {
+
     // leggo il testo inserito dall'utente
     var messaggioDaInviare = $('.testo_messaggio').val();
 
     // copio l'elemento template
     var nuovo_messaggio = $('.template .riga_messaggio.out').clone();
 
-    // inserisco il testo letto dall'input_footer
-    nuovo_messaggio.text(messaggioDaInviare);
+    // individuo l'elemento in cui devo scrivere il testo e contemporaneamente lo scrivo dentro
+    nuovo_messaggio.find('p.testo.out').text(messaggioDaInviare);
 
-    // appendo il nuovo messaggio
-    $(nuovo_messaggio).appendTo('.container_chat');
+    // appendo il nuovo messaggio nella chat
+    nuovo_messaggio.appendTo('.container_chat');
 
     // cancello l'imput
     var messaggioDaInviare = $('.testo_messaggio').val('');
 
-    // risposta automatica dopo l'invio del messaggio
-    setTimeout(function(){
-        // copio l'elemento template
-        var risposta = $('.template .riga_messaggio.in').clone();
+    // risposta automatica dopo 1 secondo dall'invio del messaggio
 
-        // inserisco il testo letto dall'input_footer
-        risposta.text('Ok!');
+    setTimeout(function(){
+        // copio l'elemento template fumetto di risposta
+        var fumetto_risposta = $('.template .riga_messaggio.in').clone();
+
+        // individuo l'elemento in cui devo scrivere il testo e contemporaneamente lo scrivo dentro
+        fumetto_risposta.find('p.testo.risposta').text('Ok!')
 
         // appendo il messaggio di risposta
-        $(risposta).appendTo('.container_chat');}, 1000);
-
-})
-
-$('.testo_messaggio').keypress(function(event){
-            var keycode = (event.keyCode ? event.keyCode : event.which);
-
-            // if (event.keyCode) {
-            //     keycode = event.keyCode;
-            // } else {
-            //     keycode = event.witch;
-            // }
-
-            if(keycode == '13'){
-                // alert('You pressed a "enter" key in textbox');
-                // leggo il testo inserito dall'utente
-                var messaggioDaInviare = $('.testo_messaggio').val();
-
-                // copio l'elemento template
-                var nuovo_messaggio = $('.template .fumetto_verde.primo').clone();
-
-                // inserisco il testo letto dall'input_footer
-                nuovo_messaggio.text(messaggioDaInviare);
-
-                // appendo il nuovo messaggio
-                $(nuovo_messaggio).appendTo('.riga_messaggio.out');
-
-                // risposta automatica dopo l'invio del messaggio
-                setTimeout(function(){
-                    // mostro il fumetto di risposta
-                    $('.risposta').show();}, 1000);
-
-            }
-})
+        fumetto_risposta.appendTo('.container_chat');}, 1000);
+}
 
 })
