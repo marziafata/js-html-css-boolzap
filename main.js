0400cb0c21582f5e9f13108476ebefe34f2a13a9
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-11 12:48:34 +0200

    messaggio

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ $(document).ready(function() {
 
 $('.invio').click(function(){
     // leggo il testo inserito dall'utente
-    var messaggio_proprietario = $('.testo_messaggio').val();
-    console.log(messaggio_proprietario);
+    var messaggioDaInviare = $('.testo_messaggio').val();
+
+    // copio l'elemento template
+    var nuovo_messaggio = $('.template .fumetto_verde.primo').clone();
+
+    // inserisco il testo letto dall'input_footer
+    nuovo_messaggio.text(messaggioDaInviare);
+
+    // appendo il nuovo messaggio
+    $(nuovo_messaggio).appendTo('.riga_messaggio.out');
 })
 
 });
