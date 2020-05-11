d752826b231d2578052d0caa1994769f606041de
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-11 12:57:18 +0200

    tasto invio

--- main.js
+++ main.js
@@ -15,4 +15,23 @@ $('.invio').click(function(){
     $(nuovo_messaggio).appendTo('.riga_messaggio.out');
 })
 
-});
+$('.testo_messaggio').keypress(function(event){
+            var keycode = (event.keyCode ? event.keyCode : event.which);
+            if(keycode == '13'){
+                // alert('You pressed a "enter" key in textbox');
+                // leggo il testo inserito dall'utente
+                var messaggioDaInviare = $('.testo_messaggio').val();
+
+                // copio l'elemento template
+                var nuovo_messaggio = $('.template .fumetto_verde.primo').clone();
+
+                // inserisco il testo letto dall'input_footer
+                nuovo_messaggio.text(messaggioDaInviare);
+
+                // appendo il nuovo messaggio
+                $(nuovo_messaggio).appendTo('.riga_messaggio.out');
+
+            }
+})
+
+})
